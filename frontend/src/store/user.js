b13d52f178b1953e6db65c6d4844637f316ba404
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-01-30 11:53:55 +0100

    Add user data to Vuex

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -3,10 +3,12 @@ import { http } from '../app';
 export default {
     namespaced: true,
     state: {
+        user: {},
         currentJwt: ''
     },
 
     getters: {
+        user: state => state.user,
         jwt: state => state.currentJwt,
         jwtData: (state, getters) => state.currentJwt ? JSON.parse(atob(getters.jwt.split('.')[1])) : null,
         jwtSubject: (state, getters) => getters.jwtData ? getters.jwtData.sub : null,
@@ -14,6 +16,9 @@ export default {
     },
 
     mutations: {
+        setUser(state, user) {
+            state.user = user;
+        },
         setJwt(state, jwt) {
             state.currentJwt = jwt;
         }
@@ -24,23 +29,29 @@ export default {
             try {
                 let jwt = localStorage.getItem('jwt');
                 let jwtIsValid = false;
+                let user = {};
 
                 if (jwt) {
-                    await http.post('/auth/validate', {}, {
+                    // If the response status isn't 200, it throws an error and is caught below
+                    const res = await http.post('/auth/validate', {}, {
                         headers: {
                             Authorization: `Bearer ${jwt}`
                         }
                     });
 
-                    // If the response status isn't 200, it throws an error and is caught below
+                    jwtIsValid = true;
+                    user = res.body.user || {};
                 }
                 else {
                     const res = await http.post('/auth/login', body);
-                    jwt = res.body.user && res.body.token ? res.body.token : null;
+                    const hasUserData = res.body.user && res.body.token;
+                    jwt = hasUserData ? res.body.token : null;
+                    user = hasUserData ? res.body.user : {};
                     jwtIsValid = jwt ? true : false;
                 }
 
                 if (jwt && jwtIsValid) {
+                    commit('setUser', user);
                     commit('setJwt', jwt);
                     localStorage.setItem('jwt', jwt);
                     return true;
