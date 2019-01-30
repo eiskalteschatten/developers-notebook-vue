9922421d0203b31a14186a4797c73ccb14b7f41d
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-01-30 13:11:42 +0100

    Add i18n to Vuetify

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueI18n from 'vue-i18n';
-import Vuetify from 'vuetify';
 import VueResource from 'vue-resource';
 import { mapState, mapActions } from 'vuex';
+
+import Vuetify from 'vuetify';
+import en from 'vuetify/es5/locale/en';
+import de from 'vuetify/es5/locale/de';
 
 import cookies from 'browser-cookies';
 
@@ -15,16 +18,22 @@ import App from './components/App.vue';
 import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
+const locale = cookies.get('preferedLanguage') || 'en';
+
 Vue.use(VueRouter);
 Vue.use(VueI18n);
 Vue.use(VueResource);
 Vue.use(Vuetify, {
     theme,
-    iconfont: 'md'
+    iconfont: 'md',
+    lang: {
+        locales: { en, de },
+        current: locale
+    }
 });
 
 const i18n = new VueI18n({
-    locale: cookies.get('preferedLanguage') || 'en',
+    locale,
     fallbackLocale: 'en'
 });
 
