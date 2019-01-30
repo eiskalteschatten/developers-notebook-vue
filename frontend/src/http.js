103502eda000f9e0a8d09075005c55e7b919a82e
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-01-30 18:27:24 +0100

    Start trying to inject the server config host into all http requests from Electron

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -7,4 +7,10 @@ Vue.http.interceptors.push(request => {
     request.headers.set('Authorization', localStorage.getItem('jwt') ? `Bearer ${localStorage.getItem('jwt')}` : '');
 });
 
+const isElectron = localStorage.getItem('isElectron');
+
+if (isElectron) {
+    Vue.http.options.root = localStorage.getItem('serverConfigHost');
+}
+
 export default Vue.http;
