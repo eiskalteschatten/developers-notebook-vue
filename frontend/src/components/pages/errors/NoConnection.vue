12e99e3f6a325b92e30949add884aec8b02a41dd
Alex <alex@alexseifert.com>
2019-02-04 00:39:36 +0100

    Start error page for when there is no connection to the server

---
+++ frontend/src/components/pages/errors/NoConnection.vue
@@ -0,0 +1,86 @@
+<i18n>
+{
+    "en": {
+        "login": "Login",
+        "username": "Username",
+        "password": "Password",
+        "loginVerb": "Log In",
+        "required": "Required",
+        "incorrectUsernameOrPassword": "Incorrect username or password."
+    },
+    "de": {
+        "login": "Anmeldung",
+        "username": "Benutzername",
+        "password": "Passwort",
+        "loginVerb": "Anmelden",
+        "required": "Erforderlich",
+        "incorrectUsernameOrPassword": "Falscher Benutzername oder falsches Passwort."
+    }
+}
+</i18n>
+
+<template>
+    <v-layout>
+        <v-flex xs12 md6 offset-md3>
+            <v-card class="mt-5">
+                <v-card-title primary-title>
+                    <div class="headline">{{ $t('login') }}</div>
+                </v-card-title>
+                <v-card-text>
+                    <v-alert
+                        :value="true"
+                        type="error"
+                        class="mb-4"
+                    >
+                        {{ $t('incorrectUsernameOrPassword') }}
+                    </v-alert>
+                </v-card-text>
+                <v-card-actions class="pl-3 pr-3 pb-3">
+                    <v-flex xs12 class="text-xs-right">
+                        <v-btn
+                            primary
+                            color="primary"
+                            @click="refresh"
+                            :loading="loading"
+                            :disabled="loading"
+                        >
+                            {{ $t('loginVerb') }}
+                        </v-btn>
+                    </v-flex>
+                </v-card-actions>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        data() {
+            return {
+                loading: false
+            };
+        },
+        created() {
+            if (this.currentJwt) {
+                this.$router.push({ name: 'home' });
+            }
+        },
+        methods: {
+            async refresh() {
+                this.loading = true;
+
+                if (loggedIn) {
+                    this.$router.replace({ name: 'home' });
+                }
+
+                this.loading = false;
+            }
+        }
+    });
+</script>
+
+<style lang="scss">
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,13 +3,14 @@ import cookies from 'browser-cookies';
 
 import MainLayout from './components/MainLayout.vue';
 import LoggedOutLayout from './components/LoggedOutLayout.vue';
-import Login from './components/pages/Login.vue';
 
 import Home from './components/pages/Home.vue';
 
+import Login from './components/pages/Login.vue';
 import ServerConfig from './components/pages/electron/ServerConfig.vue';
 
-import NotFound from './components/pages/404.vue';
+import NoConnection from './components/pages/errors/NoConnection.vue';
+import NotFound from './components/pages/errors/404.vue';
 
 const preferedLanguage = cookies.get('preferedLanguage') || 'en';
 
@@ -18,11 +19,13 @@ export const routeTitles = {
     en: {
         home: 'Dashboard',
         login: 'Login',
+        errorNoConnection: 'No Connection',
         '404': 'Page not found'
     },
     de: {
         home: 'Dashboard',
         login: 'Anmeldung',
+        errorNoConnection: 'Keine Verbindung',
         '404': 'Seite nicht gefunden'
     }
 };
@@ -50,7 +53,16 @@ export default new VueRouter({
             component: Login
         }]
     },
-    {   // Server config should not be a child of PageWrapper
+    {   // The no-connection page should not be a child of MainLayout
+        path: '/:lang/error/no-connection',
+        component: LoggedOutLayout,
+        children: [{
+            path: '',
+            name: 'errorNoConnection',
+            component: NoConnection
+        }]
+    },
+    {   // Server config should not be a child of MainLayout
         path: '/:lang/electron/server-config',
         component: LoggedOutLayout,
         children: [{
