5e0c406f2a700e05a0da110ffddc164e83ced1a0
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-02-12 17:22:13 +0100

    Started Settings page

--- frontend/src/components/pages/Settings.vue
+++ frontend/src/components/pages/Settings.vue
@@ -1,16 +1,36 @@
+<i18n>
+{
+    "en": {
+        "settings": "Settings"
+    },
+    "de": {
+        "settings": "Einstellungen"
+    }
+}
+</i18n>
+
 <template>
-    <div>
-        <h1>Developer's Notebook</h1>
-    </div>
+    <centered-column>
+        <v-card class="pa-2 mb-5">
+            <v-card-title primary-title>
+                <h1>{{ $t('settings') }}</h1>
+            </v-card-title>
+        </v-card>
+    </centered-column>
 </template>
 
 <script>
     import Vue from 'vue';
 
+    import CenteredColumn from '../elements/layout/CenteredColumn.vue';
+
     export default Vue.extend({
+        components: {
+            CenteredColumn
+        },
     });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
