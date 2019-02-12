872d0eaa0750f099320041b750aee1c160bf8cc9
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-02-12 13:40:32 +0100

    Style the about dialog

--- frontend/src/components/elements/About.vue
+++ frontend/src/components/elements/About.vue
@@ -1,10 +1,12 @@
 <i18n>
 {
     "en": {
-        "about": "About Developer's Notebook"
+        "alexseifertWebsite": "https://www.alexseifert.com",
+        "close": "Close"
     },
     "de": {
-        "about": "Über Developer's Notebook"
+        "alexseifertWebsite": "https://www.alexseifert.de",
+        "close": "Schließen"
     }
 }
 </i18n>
@@ -13,7 +15,27 @@
     <v-dialog v-model="dialog" width="500">
         <v-card>
             <v-card-text>
-                {{ $t('about') }}
+                <v-layout wrap>
+                    <v-flex xs12 md5 text-xs-center text-md-left>
+                        <img src="../../../public/images/icon.svg" class="icon">
+                    </v-flex>
+                    <v-flex xs12 md7 text-xs-center text-md-left>
+                        <v-layout wrap fill-height>
+                            <v-flex mt-2>
+                                <h2>Developer's Notebook</h2>
+                                <div class="by mb-4">by <external-link :href="$t('alexseifertWebsite')" link-content="Alex Seifert" /></div>
+
+                                <div class="legal">
+                                    &copy; {{ copyrightYear }} Alex Seifert
+                                </div>
+                            </v-flex>
+
+                            <v-flex xs12 text-md-right align-end mt-4>
+                                <v-btn flat @click="dialog = false">{{ $t('close') }}</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-flex>
+                </v-layout>
             </v-card-text>
         </v-card>
     </v-dialog>
@@ -23,11 +45,22 @@
     import Vue from 'vue';
     import eventBus from '../../eventBus';
 
+    import ExternalLink from './ExternalLink.vue';
+
     export default Vue.extend({
+        components: {
+            ExternalLink
+        },
         data() {
             return {
                 dialog: false
             };
+        },
+        computed: {
+            copyrightYear() {
+                const year = new Date().getFullYear();
+                return year === 2019 ? year : `2019 - ${year}`;
+            }
         },
         created() {
             eventBus.$on('open-modal', () => {
@@ -40,6 +73,17 @@
     });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    .icon {
+        height: 175px;
+        width: 175px;
+    }
 
+    .by {
+        font-size: .9em;
+    }
+
+    .legal {
+        font-size: .8em;
+    }
 </style>
