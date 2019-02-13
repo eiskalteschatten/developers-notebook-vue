6be750fd98039a3f8d2c0de19d6debe648d72e9d
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-02-13 10:00:33 +0100

    Add theme boxes to settings

--- frontend/src/components/pages/Settings.vue
+++ frontend/src/components/pages/Settings.vue
@@ -1,10 +1,16 @@
 <i18n>
 {
     "en": {
-        "settings": "Settings"
+        "settings": "Settings",
+        "theme": "Theme",
+        "light": "Light",
+        "dark": "Dark"
     },
     "de": {
-        "settings": "Einstellungen"
+        "settings": "Einstellungen",
+        "theme": "Erscheinungsbild",
+        "light": "Hell",
+        "dark": "Dunkel"
     }
 }
 </i18n>
@@ -15,6 +21,26 @@
             <v-card-title primary-title>
                 <h1>{{ $t('settings') }}</h1>
             </v-card-title>
+            <v-card-text>
+
+                <div class="mb-3">
+                    <h3>{{ $t('theme') }}</h3>
+
+                    <div class="preferences-theme mt-3 mr-5" data-theme="light">
+                        <div class="theme-image light">
+                            <div class="gradient"></div>
+                        </div>
+                        {{ $t('light') }}
+                    </div>
+                    <div class="preferences-theme" data-theme="dark">
+                        <div class="theme-image dark">
+                            <div class="gradient"></div>
+                        </div>
+                        {{ $t('dark') }}
+                    </div>
+                </div>
+
+            </v-card-text>
         </v-card>
     </centered-column>
 </template>
@@ -32,5 +58,42 @@
 </script>
 
 <style lang="scss" scoped>
+    .preferences-theme {
+        cursor: pointer;
+        display: inline-block;
+        text-align: center;
 
+        .theme-image {
+            $borderSize: 2px;
+            height: 75px;
+            margin-bottom: 10px;
+            width: 100%;
+
+            &.light {
+                .gradient {
+                    background-image: linear-gradient(to right, #000000 10%, #f3f3f3 10%, #f3f3f3 90%);
+                }
+            }
+
+            &.dark {
+                .gradient {
+                    background-image: linear-gradient(to right, #000000 10%, #252525 10%, #252525 90%);
+                }
+            }
+
+            &.selected {
+                .gradient {
+                    border: 1px solid #0e0e0e;
+                }
+            }
+
+            .gradient {
+                border: 1px solid rgba(0, 0, 0, 0.3);
+                border-radius: 3px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+                height: 100%;
+                width: 120px;
+            }
+        }
+    }
 </style>
