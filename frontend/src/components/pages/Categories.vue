8402ffdbe9ed59bf1213ee9373aa5ac2c853a3ef
Alex <alex@alexseifert.com>
2019-02-19 23:27:50 +0100

    Started new category dialog

--- frontend/src/components/pages/Categories.vue
+++ frontend/src/components/pages/Categories.vue
@@ -1,10 +1,16 @@
 <i18n>
 {
     "en": {
-        "categories": "Categories"
+        "categories": "Categories",
+        "newCategory": "New Category",
+        "create": "Create",
+        "cancel": "Cancel"
     },
     "de": {
-        "categories": "Kategorien"
+        "categories": "Kategorien",
+        "newCategory": "Neue Kategorie",
+        "create": "Erstellen",
+        "cancel": "Abbrechen"
     }
 }
 </i18n>
@@ -13,24 +19,62 @@
     <div>
         <h1 class="mb-4">{{ $t('categories') }}</h1>
 
-        test
+        <v-layout wrap>
+            <v-flex xs12 md2>
+                <v-btn @click="newCategoryDialog = true">
+                    {{ $t('newCategory') }}
+                </v-btn>
+            </v-flex>
+            <v-flex xs12 md10>
+                test
+            </v-flex>
+        </v-layout>
+
+        <v-dialog v-model="newCategoryDialog" max-width="600">
+            <v-card>
+                <v-card-title class="headline">{{ $t('newCategory') }}</v-card-title>
+
+                <v-card-text>
+                    <edit-category-form />
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer />
+
+                    <v-btn flat="flat" @click="newCategoryDialog = false">
+                        {{ $t('cancel') }}
+                    </v-btn>
+
+                    <v-btn color="primary" @click="createNewCategory">
+                        {{ $t('create') }}
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
 <script>
     import Vue from 'vue';
 
+    import EditCategoryForm from './categories/EditCategoryForm.vue';
+
     export default Vue.extend({
+        components: {
+            EditCategoryForm
+        },
         data() {
             return {
-                drawer: !this.$vuetify.breakpoint.smAndDown
+                newCategoryDialog: false
             };
+        },
+        methods: {
+            createNewCategory() {
+                this.newCategoryDialog = false;
+            }
         }
     });
 </script>
 
 <style lang="scss" scoped>
-    .drawer {
-        float: right;
-    }
 </style>
---
+++ frontend/src/components/pages/categories/EditCategoryForm.vue
@@ -0,0 +1,62 @@
+<i18n>
+{
+    "en": {
+        "newCategory": "New Category",
+        "name": "Name",
+        "slug": "Slug",
+        "automaticallyGenerated": "automatically generated if left empty",
+        "description": "Description",
+        "color": "Color",
+        "parentCategory": "Parent Category"
+    },
+    "de": {
+        "newCategory": "Neue Kategorie",
+        "name": "Name",
+        "slug": "Slug",
+        "automaticallyGenerated": "wird automatisch generiert, wenn leer gelassen",
+        "description": "Beschreibung",
+        "color": "Farbe",
+        "parentCategory": "Oberkategorie"
+    }
+}
+</i18n>
+
+<template>
+    <v-container grid-list-md>
+        <v-text-field v-model="editCategory.name" :label="$t('name')" />
+        <v-text-field v-model="editCategory.slug" :label="$t('slug')" :hint="$t('automaticallyGenerated')" persistent-hint />
+        <v-textarea v-model="editCategory.description" :label="$t('description')" />
+        <v-text-field v-model="editCategory.parentCategory" :label="$t('parentCategory')" />
+        <v-subheader>{{ $t('color') }}</v-subheader>
+
+        <swatches
+            v-model="editCategory.color"
+            inline
+            colors="material-basic"
+        />
+    </v-container>
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    import Swatches from 'vue-swatches';
+    import 'vue-swatches/dist/vue-swatches.min.css';
+
+    export default Vue.extend({
+        components: {
+            Swatches
+        },
+        data() {
+            return {
+                editCategory: {}
+            };
+        }
+    });
+</script>
+
+<style lang="scss">
+    .vue-swatches__container {
+        background-color: transparent !important;
+    }
+</style>
