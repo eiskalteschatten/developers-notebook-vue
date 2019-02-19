6cedf5fd9a79e9f33c0bc6a5c80ddad6d1a4a800
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-02-19 17:41:57 +0100

    Start categories page

--- frontend/src/components/pages/Categories.vue
+++ frontend/src/components/pages/Categories.vue
@@ -1,6 +1,19 @@
+<i18n>
+{
+    "en": {
+        "categories": "Categories"
+    },
+    "de": {
+        "categories": "Kategorien"
+    }
+}
+</i18n>
+
 <template>
     <div>
-        <h1>Developer's Notebook</h1>
+        <h1 class="mb-4">{{ $t('categories') }}</h1>
+
+        test
     </div>
 </template>
 
@@ -8,9 +21,16 @@
     import Vue from 'vue';
 
     export default Vue.extend({
+        data() {
+            return {
+                drawer: !this.$vuetify.breakpoint.smAndDown
+            };
+        }
     });
 </script>
 
 <style lang="scss" scoped>
-
+    .drawer {
+        float: right;
+    }
 </style>
