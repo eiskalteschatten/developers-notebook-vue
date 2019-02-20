7a516b2ab271e2dc3de9e036d0773eae57f8bca8
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2019-02-20 14:47:57 +0100

    Clean up unused functions

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -5,8 +5,6 @@ const {db} = require('../lib/db');
 
 const User = require('./User');
 
-const paginationLimit = 50;
-
 const Category = db.define('category', {
     name: Sequelize.STRING,
     slug: Sequelize.STRING,
@@ -18,46 +16,6 @@ const Category = db.define('category', {
 
 User.hasMany(Category);
 Category.belongsTo(User);
-
-
-Category.getPaginatedSorted = async function(page, websiteId, sortBy = 'name', sortDirection = 'ASC') {
-    const models = await this.findAndCountAll({
-        where: { websiteId },
-        order: [
-            [ sortBy, sortDirection ]
-        ],
-        limit: paginationLimit,
-        offset: paginationLimit * (page - 1)
-    });
-
-    return {
-        model: models,
-        raw: models.rows
-    };
-};
-
-Category.getPaginatedSortedSearch = async function(page, websiteId, sortBy = 'name', sortDirection = 'ASC', query = '') {
-    const models = await this.findAndCountAll({
-        attributes: [ 'id' ],
-        where: {
-            websiteId,
-            $or: [
-                { name: { $iLike: `%${query}%` } },
-                { description: { $iLike: `%${query}%` } }
-            ]
-        },
-        order: [
-            [ sortBy, sortDirection ]
-        ],
-        limit: paginationLimit,
-        offset: paginationLimit * (page - 1)
-    });
-
-    return {
-        model: models,
-        raw: models.rows
-    };
-};
 
 Category.sync();
 
