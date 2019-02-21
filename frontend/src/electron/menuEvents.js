1eebf471e2bf27df6dae5bc8c9ee09686bc1b07d
Alex <alex@alexseifert.com>
2019-02-21 13:30:53 +0100

    Fix a bug when navigating away from the new category dialog in Electron using the app menu

--- frontend/src/electron/menuEvents.js
+++ frontend/src/electron/menuEvents.js
@@ -5,8 +5,20 @@ import router from '../router';
 
 
 export default () => {
-    ipcRenderer.on('open-route-name', (event, name, { query, params }) => {
-        router.push({ name, query, params });
+    ipcRenderer.on('open-route-name', (event, name, vars = {}) => {
+        const route = router.app.$route;
+
+        router.push({
+            name,
+            query: {
+                ...route.query,
+                ...vars.query
+            },
+            params: {
+                ...route.params,
+                ...vars.params
+            }
+        });
     });
 
     ipcRenderer.on('open-modal', (event, modal) => {
